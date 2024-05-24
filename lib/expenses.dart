cb34db48be53db98244cd94c1602d798ceda55da
JuaniV2002 <juani2002villa@gmail.com>
2024-05-24 16:15:09 -0300

    complete remake of app, added expense model

---
+++ lib/expenses.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpenseState();
+  }
+}
+
+class _ExpenseState extends State<Expenses> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Column(
+      children: const [
+        Text('The Chart'),
+        Text('The List of Transactions'),
+      ],
+    ),);
+  }
+}
